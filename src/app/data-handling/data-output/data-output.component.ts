b53c35346d8ee01d57dc0a21a18f67cbce67b0fd
Ba1bhav <125265307+Ba1bhav@users.noreply.github.com>
2023-03-20 19:15:19 +0000

    edit

--- src/app/data-handling/data-output/data-output.component.ts
+++ src/app/data-handling/data-output/data-output.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { ToastrService } from 'src/app/toastr.service';
 import { RequestsHandlerService } from 'src/app/requests-handler.service';
+import { taskStatusButtons } from 'src/assets/Buttons';
 @Component({
   selector: 'app-data-output',
   templateUrl: './data-output.component.html',
   styleUrls: ['./data-output.component.css']
 })
 export class DataOutputComponent {
+  taskStatuses=taskStatusButtons;
   TasksFetched:any=[];
   ArrayofIds:any=[];
+  editToggle:boolean=false;
   constructor(private toastr:ToastrService,private httpHandler:RequestsHandlerService){
     this.httpHandler.getTasks().subscribe((response:any)=>{
       this.toastr.emitSuccess('Data Fetched Successfully !')
@@ -19,5 +22,12 @@ export class DataOutputComponent {
       console.log(this.TasksFetched)
     },()=>{this.toastr.emitError('Some Error Occured !')})
   }
-
+  editTasks(){
+   if(this.editToggle){
+    this.editToggle=false;
+   }
+   else{
+    this.editToggle=true;
+   }
+  }
 }
---
+++ src/assets/Buttons.ts
@@ -0,0 +1,7 @@
+const taskStatusButtons=[
+   {'name':'In-Progress','value':1},
+   {'name':'Not-Started','value':0},
+   {'name':'Done','value':2},
+   {'name':'Halt','value':3}
+]
+export{taskStatusButtons}
